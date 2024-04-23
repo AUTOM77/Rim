1a2ed164c11b291903547e80ee2de231141dbe6b
Monius <m1105930979@gmail.com>
2024-04-24 02:06:16 +0800

    init

---
+++ rimc/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn single_cap(f: &str, conf: String) {
+    println!("Processing file: {} with {}", f, conf);
+}
+
+pub fn batch_cap(d: &str, conf: String) {
+    println!("Processing directory: {} with {}", d, conf);
+}
